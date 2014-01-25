f4a93320bfaff66f0d04d55cdb6f58eb4607cc8c
Adam Midvidy <amidvidy@gmail.com>
2014-01-25 11:36:24 -0800

    add steg fn

--- app/app.py
+++ app/app.py
@@ -1,6 +1,19 @@
 from __future__ import print_function
 
+import sys
+import stepic
+
 from flask import Flask
+from PIL import Image
+
+# much image, such steg
+# ...
+# ...
+# Wooooooow
+def encode_image(imagefile, message, outfile):
+    image = Image.open(imagefile)
+    encoded = stepic.encode(image, message)
+    return encoded
 
 def main():
     print("Hackathon winning application right here")
