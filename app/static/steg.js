f8f8b62b602ae598dfc4a10031ad94cf16ca7f43
Adam Midvidy <amidvidy@gmail.com>
2014-01-25 19:41:49 -0800

    add steg img to page

--- app/static/steg.js
+++ app/static/steg.js
@@ -1,4 +1,33 @@
+var stegApp = (function(document, window, $) {
+
+    var module = {};
+
+    module.displayRawImage = function(imageUrl) {
+        var $imageWrapper = $("#image-wrapper");
+        var $image = $("<img/>")
+            .attr("src", imageUrl)
+            .attr("width", "100%");
+
+        // Load image, then add to page
+        $image.load(function() {
+            $imageWrapper.append($(this));
+
+            // Expand message box height to image height
+            $messageBox = $("#message");
+
+            $messageBox.height($(this).height());
+        });
+
+    };
+
+    return module;
+
+}(document, window, jQuery));
+
 $(document).ready(function() {
+
+    var state = {};
+
     // capture message length
     $("#message").keydown( function(e){
         $("#len").text("(length: " + $("#message").val().length + ")");
@@ -7,12 +36,15 @@ $(document).ready(function() {
 
     var choose_opts = {
         success: function(files) {
-            alert("Here's the file link: " + files[0].link);
+            var rawImageUrl = files[0].link;
+            state.rawImageUrl = rawImageUrl;
+            console.log(rawImageUrl);
+            stegApp.displayRawImage(rawImageUrl);
         },
 
         cancel: function() {},
 
-        linkType: "preview",
+        linkType: "direct",
 
         multiselect: false,
 
