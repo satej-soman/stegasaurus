d96c79e4988c87be08e1940bc555f2ebb75c4d48
Adam Midvidy <amidvidy@gmail.com>
2014-01-25 18:02:37 -0800

    add dbx boilerplate

--- app/static/steg.js
+++ app/static/steg.js
@@ -4,4 +4,22 @@ $(document).ready(function() {
         $("#len").text("(length: " + $("#message").val().length + ")");
         console.log("*");
     });
+
+    var choose_opts = {
+        success: function(files) {
+            alert("Here's the file link: " + files[0].link);
+        },
+
+        cancel: function() {},
+
+        linkType: "preview",
+
+        multiselect: false,
+
+        extensions: [".jpg", ".jpeg", ".png", ".bmp", ".tiff"]
+    };
+
+    $("#photo-select").click(function(e) {
+        Dropbox.choose(choose_opts);
+    });
 });
